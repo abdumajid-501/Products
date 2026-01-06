956f27b09950596db9a4b4ebbb8e94d2e03f8b97
abdumajid-501 <dasturchi501@gmail.com>
2026-01-06 10:24:13 +0500

    .

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ function Footer() {
   return (
     <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
       <aside>
-        <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+        <p>Mualliflik huquqi © {new Date().getFullYear()} — Barcha huquqlar ACME Industries Ltd tomonidan himoyalangan</p>
+
       </aside>
     </footer>  )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-md">
           <div className="container flex items-center justify-between">
-              <Link to={"/"} className="btn btn-ghost text-xl mr-auto">Products</Link>
+              <Link to={"/"} className="btn btn-ghost text-xl mr-auto">Maxsulotlar</Link>
         <Link to={"/cart"} className='btn relative'>
           <span className='absolute top-0 right-1 text-white bg-red-600 rounded-full px-1.5'>{cartProducts.length > 0 ? cartProducts.length : " "}</span>
               <IoMdCart size={24} />
